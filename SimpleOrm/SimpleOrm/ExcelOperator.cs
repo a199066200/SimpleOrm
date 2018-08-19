75db4e3cbe808ed2a5622c984844716c8fa74f2f
jazzzz <693376807@qq.com>
2018-08-19 16:13:44 +0800

    添加SimpleOrm. SimpleOrm is a simple way to read and write excel use oledb

---
+++ SimpleOrm/SimpleOrm/ExcelOperator.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOrm
+{
+    public class ExcelOperator
+    {
+        /// <summary>
+        /// 读取excel文件中指定名字的sheet中的全部数据       
+        /// 异常：  
+        ///      ArgumentNullException: path不存在或者指定的文件不是 excel文件
+        ///      
+        ///     System.Data.OleDb.OleDbException:打开连接时出现的连接级别错误。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="sheetName"></param>
+        /// <returns></returns>
+        public static DataTable QueryAll(string path, string sheetName)
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection cn = BuildConnection(path);
+            try
+            {
+                TryOpenConnection(cn);
+                var cmd = new OleDbCommand("select * from [" + sheetName + "$] where F1 is to null", cn);
+                var apt = new OleDbDataAdapter(cmd);
+                apt.Fill(dt);
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+            if (dt.Rows.Count == 0 || dt.Columns.Count == 0)
+            {
+                throw new Exception(string.Format("sheet: '{0}' doesn't exist any data", sheetName));
+            }
+            return dt;
+        }
+
+        /// <summary>
+        /// 将 table 写入到 path中
+        /// 异常：
+        ///      InvalidOperationException: path不存在或者指定的文件不是 excel文件       
+        ///      OleDbException:打开连接时出现的连接级别错误。
+        ///      ArgumentNullException：table is null or empty.
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="path"></param>
+        public static void WriteOnNewSheet(DataTable table, string path)
+        {
+            if (table == null|| table.Rows.Count == 0 || table.Columns.Count == 0)
+            {
+                throw new ArgumentNullException("table is null or empty");
+            }
+            var cn = BuildConnection(path);
+            try
+            {
+                TryOpenConnection(cn);
+                var sql = new StringBuilder().Append("CREATE TABLE [" + table.TableName + "]");
+                sql.Append("(");
+                foreach (DataColumn column in table.Columns)
+                {
+                    sql.Append("[" + column.ColumnName + "] text,");
+                }
+                sql.Remove(sql.Length - 1, 1);
+                sql.Append(")");
+                var cmd = new OleDbCommand(sql.ToString(), cn);
+                cmd.ExecuteNonQuery();
+                StringBuilder sqlValue = new StringBuilder();
+                foreach (DataRow row in table.Rows)
+                {
+                    sql.Clear();
+                    sql.Append("INSERT INTO [" + table.TableName + "] values (");
+                    foreach (DataColumn column in table.Columns)
+                    {
+                        sql.Append(row[column].ToString() + ",");
+                    }
+                    sql.Remove(sql.Length - 1, 1);
+                    sql.Append(")");
+                    cmd.CommandText = sql.ToString();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            finally
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// 使用path创建一个 Excel连接
+        /// 异常：
+        ///     ArgumentNullException:path 不存在或者指定的文件不是 excel文件， 
+        ///     
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static OleDbConnection BuildConnection(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException("path is not valid");
+            }
+            if (!File.Exists(path))
+            {
+                throw new InvalidOperationException(string.Format("the path:{0} is not exist", path));
+            }
+            var extension = Path.GetExtension(path);
+            if (extension != ".xls" || extension != ".xlsx")
+            {
+                throw new InvalidOperationException(string.Format("path:{0}, the extension,{1}, is  not .xls or .xlsx, invalid operation", path, extension));
+            }
+            string cnString;
+            if (extension == ".xls")
+            {
+                cnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";Extended Properties='Excel 8.0;HDR=no;IMEX=1;'";
+            }
+            else
+            {
+                cnString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties='Excel 12.0;HDR=no;IMEX=1;'";
+            }
+            return new OleDbConnection(cnString);
+        }
+
+        /// <summary>
+        /// 尝试打开连接，如果连接已经打开，不会抛出 InvalidOperationException
+        /// </summary>
+        /// <param name="cn"></param>
+        private static void TryOpenConnection(OleDbConnection cn)
+        {
+            try
+            {
+                cn.Open();
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+        }
+
+    }    
+}
---
+++ SimpleOrm/SimpleOrm/SimpleOrm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOrm
+{
+    public static class SimpleOrm
+    {
+        static SimpleOrm()
+        {
+            var typeName = "DynamicType";
+            var an = new AssemblyName(typeName);
+            AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+            _mb = ab.DefineDynamicModule("Dynamic Module");
+        }
+
+        /// <summary>
+        /// 从data中读取数据，转化为动态类型的数据
+        /// 异常：
+        ///     ArgumentNullException :data为null
+        ///     InvalidOperationException:can't find table from data
+        ///     
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public static IEnumerable<dynamic> Read(this DataSet data, string tableName)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException("Can't read, param 'data' is null");
+            }
+            DataTable table;
+            if (!TryGetTargetTable(data, tableName, out table))
+            {
+                throw new InvalidOperationException($"can't find table {tableName} from given Datatable 'data'");
+            }
+            return Read(table);
+        }
+
+        /// <summary>
+        /// 从data中读取数据，转化为动态类型的数据
+        /// 异常：
+        ///     ArgumentNullException :table为null
+        /// </summary>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        public static IEnumerable<dynamic> Read(this DataTable table)
+        {
+            if (table == null || table.Columns.Count == 0 || table.Rows.Count == 0)
+            {
+                throw new ArgumentNullException("Can't read, param 'table' is null or empty");
+            }
+            var deserializer = BuildDeserializer(GetNameColumnPair(table));
+            foreach (DataRow row in table.Rows)
+            {
+                yield return deserializer(row);
+            }
+        }
+
+        /// <summary>
+        /// 将 list 转换为 DataTable
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static DataTable BuildDataTable(object[] list)
+        {
+            if (list == null || !list.Any())
+            {
+                throw new ArgumentNullException("argument list is null or empty");
+            }
+            var properties = list[0].GetType().GetProperties().OrderBy(p=>p.Name);
+            var columns = properties.Select(p => new DataColumn(p.Name, p.DeclaringType)).OrderBy(p=>p.ColumnName);
+            var table = new DataTable();
+            foreach (var column in columns)
+            {
+                table.Columns.Add(column);
+            }
+            foreach (var item in list)
+            {
+                table.LoadDataRow(GetValues(item, properties), false);
+            }
+            return table;
+        }
+
+        private static bool TryGetTargetTable(DataSet data, string tableName, out DataTable table)
+        {
+            foreach (DataTable item in data.Tables)
+            {
+                if (item.TableName == tableName)
+                {
+                    table = item;
+                    return true;
+                }
+            }
+            table = null;
+            return false;
+        }
+
+        private static IEnumerable<string> GetTableColumnsName(DataTable table)
+        {
+            foreach (DataColumn column in table.Columns)
+            {
+                yield return column.ColumnName;
+            }
+        }
+
+        private static IEnumerable<KeyValuePair<string, DataColumn>> GetNameColumnPair(DataTable table)
+        {
+            foreach (DataColumn column in table.Columns)
+            {
+                yield return new KeyValuePair<string, DataColumn>(column.ColumnName, column);
+            }
+        }
+
+        private static Func<DataRow, dynamic> BuildDeserializer(IEnumerable<KeyValuePair<string, DataColumn>> pairs)
+        {
+            var type = BuildType(pairs);
+            return new Func<DataRow, dynamic>(row =>
+            {
+                object o = Activator.CreateInstance(type);
+                foreach (var p in pairs)
+                {
+                    var property = type.GetProperty(p.Key);
+                    property.SetValue(o, row[p.Value]);
+                }
+                return o;
+            });
+        }
+
+        static ModuleBuilder _mb;
+        static int _typeVersion = 0;
+        private static Type BuildType(IEnumerable<KeyValuePair<string, DataColumn>> pairs)
+        {
+            var name = "type" + _typeVersion;
+            _typeVersion += 1;
+            var tb = _mb.DefineType(name, TypeAttributes.Public);
+            foreach (var pair in pairs)
+            {
+                tb.DefineField(pair.Key, pair.Value.DataType, FieldAttributes.Public);
+            }
+            return tb.CreateType();
+        }
+
+        private static object[] GetValues(object o, IEnumerable<PropertyInfo> properties)
+        {
+            var values = new List<object>();
+            foreach (var p in properties)
+            {
+                values.Add(p.GetValue(o));
+            }
+            return values.ToArray();
+        }
+    }
+}
